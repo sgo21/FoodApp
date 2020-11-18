052f4e487fd057545bc116728b918e0f07b3e10d
nehajakk <54371588+nehajakk@users.noreply.github.com>
2020-11-18 01:22:26 -0500

    initial commit

---
+++ src/components/Recipe.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import RecipeDetails from "./RecipeDetails";
+
+const Recipe = ({ recipe }) => {
+  const [show, setShow] = useState(false);
+  const { label, image, url, ingredients, calories, dietLabels, healthLabels} = recipe.recipe;
+
+  return (
+    <div className="recipe">
+      <h2>{label}</h2>
+      <img src={image} alt={label} />
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        View Recipe
+      </a>
+      <h3> {Math.round(calories)} Calories </h3>
+      <h3> {dietLabels} </h3>
+      <h3> {healthLabels} </h3>
+      {/* <h4> {totalNutrients} </h4> */}
+      <button onClick={() => setShow(!show)}>Ingredients</button>
+      {show && <RecipeDetails ingredients={ingredients} />}
+    </div>
+  );
+};
+
+export default Recipe;
