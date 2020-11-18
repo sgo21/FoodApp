0e8fc0f34b4a708dfcdc389ff540e4e92e2af307
sgo21 <54879349+sgo21@users.noreply.github.com>
2020-11-18 03:53:53 -0500

    added nav bar and page components
    added navigation bar and page components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ import { v4 as uuidv4 } from "uuid";
 import Recipe from "./components/Recipe";
 import Alert from "./components/Alert";
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import React, { Component } from 'react';
+
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Error from './components/Error';
+import Navigation from './components/Navigation';
+
+
+
 function App() {
   const [query, setQuery] = useState("");
   const [recipes, setRecipes] = useState([]);
@@ -40,22 +51,44 @@ function App() {
   return (
     <div className="App">
       <h1>Food Searching App</h1>
-      <form onSubmit={onSubmit} className="search-form">
-        {alert !== "" && <Alert alert={alert} />}
-        <input
-          type="text"
-          name="query"
-          onChange={onChange}
-          value={query}
-          autoComplete="off"
-          placeholder="Search Food"
-        />
-        <input type="submit" value="Search" />
-      </form>
-      <div className="recipes">
-        {recipes !== [] &&
-          recipes.map(recipe => <Recipe key={uuidv4()} recipe={recipe} />)}
+
+      <BrowserRouter>
+        <div>
+          <Navigation />
+            <Switch>
+             <Route path="/" component={Home} exact/>
+             <Route path="/about" component={About}/>
+             <Route path="/contact" component={Contact}/>
+            <Route component={Error}/>
+           </Switch>
+        </div> 
+      </BrowserRouter>
+
+      <div>
+        <div className="nav-bar">
+          <a href="./Home">Home</a>
+          <a href="./LogIn">LogIn</a>
+          <a href="./savedmeals">My Saved Meals</a>
+          <a href="./progress">My Progress</a>
+        </div>
       </div>
+
+        <form onSubmit={onSubmit} className="search-form">
+          {alert !== "" && <Alert alert={alert} />}
+          <input
+            type="text"
+            name="query"
+            onChange={onChange}
+            value={query}
+            autoComplete="off"
+            placeholder="Search Food"
+          />
+          <input type="submit" value="Search" />
+        </form>
+        <div className="recipes">
+          {recipes !== [] &&
+            recipes.map(recipe => <Recipe key={uuidv4()} recipe={recipe} />)}
+        </div>
     </div>
   );
 }
